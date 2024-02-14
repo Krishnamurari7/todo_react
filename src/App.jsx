20e9765f935ea946adcd97169db796a8130f2efa
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-02-14 15:21:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="container">
-        <div className="bg-red-600">
-          Todo list
+      <Navbar  />
+      <div className="container mx-auto my-5 rounded-xl bg-violet-100 min-h-[80vh]">
+        <div className="addtodo my-5">
+          <h2 className='text-lg font-bold'>Add Todo</h2>
+          <input type='text' className='w-2/4' />
+          <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6'>Add</button>
         </div>
+      
+          <h2 className='text-lg font-bold'>My Todos</h2>
+          <div className="todos">
+            <div className="todo flex">
+              <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, adipisci.</div>
+              <div className="buttons">
+                <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
+                <button onClick={handledelete} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
+              </div>
+
+            </div>
+          </div>
       </div>
     </>
   )
